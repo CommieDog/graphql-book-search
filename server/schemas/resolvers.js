66fe8d766aedba85d9e71c63dce39181aa222cc0
John Netzel <johnrnetzel@gmail.com>
2022-06-03 06:44:34 -0800

    Add import for server resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -6,10 +6,14 @@ const resolvers = {
     Query: {
         me: async (parent, args) => {
             return await User.findById(args.id);
+        },
+        test: async () => {
+            return "Test"
         }
     },
     Mutation: {
         login: async (parent, args) => {
+            console.log(args);
             const user = await User.findOne({ email: args.email });
             const passwordMatches = await user.isCorrectPassword(args.password);
             if(!passwordMatches)
@@ -20,9 +24,11 @@ const resolvers = {
             return { token: token, user: user}
         },
         addUser: async (parent, args) => {
-            const user = await User.create({ username: args.username, email: args.email, password: args.password });
-            const token = signToken(user);
-            return { token: token, user: user}
+            //const user = await User.create({ username: args.username, email: args.email, password: args.password });
+            //const token = signToken(user);
+            //return { token: token, user: user};
+            //console.log("ABCD");
+            //return "ABCD";
         },
         saveBook: async (parent, args) => {
             return User.findOneAndUpdate(
@@ -40,3 +46,5 @@ const resolvers = {
         }
     }
 }
+
+module.exports = resolvers;
