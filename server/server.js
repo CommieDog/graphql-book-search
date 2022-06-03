be5f100a6aaf63e5b411ce67d96674a4dfe53ee9
John Netzel <johnrnetzel@gmail.com>
2022-06-02 18:41:03 -0800

    Convert server.js to use Apollo server

--- server/server.js
+++ server/server.js
@@ -3,6 +3,11 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -14,8 +19,26 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+/*app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+});*/
+
+// Create a new instance of an Apollo server with the GraphQL schema
+const startApolloServer = async (typeDefs, resolvers) => {
+    await server.start();
+
+    // Update Express.js to use Apollo server features
+    server.applyMiddleware({ app });
+
+    db.once('open', () => {
+        app.listen(PORT, () => {
+            console.log(`API server running on port ${PORT}!`);
+            console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+        })
+    });
+};
+  
+  // Call the async function to start the server
+  startApolloServer(typeDefs, resolvers);
