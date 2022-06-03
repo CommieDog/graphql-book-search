57979fac50450b82397717d8ec4ffed49cf33fe4
John Netzel <johnrnetzel@gmail.com>
2022-06-02 18:40:26 -0800

    Fix input typeDef used as object

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -9,7 +9,16 @@ const typeDefs = gql`
         savedBooks: [Book]
     }
 
-    input Book {
+    type Book {
+        bookId: String!
+        authors: [String]
+        description: String!
+        title: String!
+        image: String
+        link: String
+    }
+
+    input BookInput {
         bookId: String!
         authors: [String]
         description: String!
@@ -30,7 +39,9 @@ const typeDefs = gql`
     type Mutation {
         login(email: String!, password: String!): Auth
         addUser(username: String!, email: String!, password: String!): Auth
-        saveBook(book: Book!): User
+        saveBook(book: BookInput!): User
         removeBook(bookId: ID!): User
     }
 `;
+
+module.exports = typeDefs;
